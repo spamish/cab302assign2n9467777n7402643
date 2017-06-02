c1dfb9ffc5aab0b6879a7c03b48f63d84aed3bc0
adambona <adambona6@gmail.com>
2017-06-02 19:12:03 +1000

    Few tests done, working op pizza classes now
    CustomerFactoryTests done(check correct)

--- src/asgn2Tests/CustomerFactoryTests.java
+++ src/asgn2Tests/CustomerFactoryTests.java
@@ -1,5 +1,13 @@
 package asgn2Tests;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import asgn2Customers.CustomerFactory;
+import asgn2Customers.Customer;
+import asgn2Exceptions.CustomerException;
+
+
 /**
  * A class the that tests the asgn2Customers.CustomerFactory class.
  * 
@@ -7,5 +15,34 @@ package asgn2Tests;
  *
  */
 public class CustomerFactoryTests {
-	// TO DO
+	String name;
+	String mobileNumber;
+	int locationX;
+	int locationY;
+	Customer customer;
+	
+	//Setup
+	
+	@Before
+	public void ReferenceCustomer() throws CustomerException {
+		name = "Adam";
+		mobileNumber = "0465734365";
+		locationX = 3;
+		locationY = 2;
+	}
+	
+	//Tests
+	
+	@Test
+	public void getCustomer() throws CustomerException {
+		String temp = "DVC";
+		
+		try {
+			customer = CustomerFactory.getCustomer(temp, name, mobileNumber, locationX, locationY);
+		} catch (CustomerException e) {
+			e.printStackTrace();
+		}
+
+	}		
+
 }
