0b8e0c3fb6fab125310b1678da7617863a6f6ba5
Samuel Janetzki <samueljanetzki@gmail.com>
2017-06-02 21:15:53 +1000

    Gui Working, something happened so making another coomit before pushing changes to master

--- src/asgn2Tests/CustomerTests.java
+++ src/asgn2Tests/CustomerTests.java
@@ -1,6 +1,16 @@
 package asgn2Tests;
 
+import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Test;
+import asgn2Exceptions.CustomerException;
+import asgn2Exceptions.LogHandlerException;
+import asgn2Customers.Customer;
+import asgn2Customers.DriverDeliveryCustomer;
+import asgn2Customers.DroneDeliveryCustomer;
+import asgn2Customers.PickUpCustomer;
+import asgn2Restaurant.LogHandler;
 /**
  * A class that tests the that tests the asgn2Customers.PickUpCustomer, asgn2Customers.DriverDeliveryCustomer,
  * asgn2Customers.DroneDeliveryCustomer classes. Note that an instance of asgn2Customers.DriverDeliveryCustomer 
@@ -11,5 +21,30 @@ package asgn2Tests;
  *
  */
 public class CustomerTests {
-	// TO DO
+
+
+	
+	
+	@Before 
+	public void ReferenceCustomer() throws CustomerException {
+	
+				
+	}
+	
+	
+	
+	@Test
+	public void DriverDeliveryCustomer() throws CustomerException {
+		
+	}
+	
+	@Test
+	public void DroneDeliveryCustomer() throws CustomerException {
+		
+	}
+	
+	@Test
+	public void PickUpCustomer() throws CustomerException {
+		
+	}
 }
